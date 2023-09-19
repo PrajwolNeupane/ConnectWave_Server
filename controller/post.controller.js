49ea94e268d1346319f776ad87bd33f648f4a28c
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-09-19 11:37:48 +0545

    feat:added uploading images and videos while uploading post

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -24,36 +24,66 @@ export const uploadPost = async (req, res) => {
   try {
     if (req.files?.length > 0) {
       for (let i = 0; i < req.files.length; i++) {
-        if (req.files[i].size > 200 * 1024) {
-          return res.status(400).send({
-            message: "File size exceeds the limit of 200KB",
-            success: false,
-          });
+        if (req.files[i].mimetype.includes("image")) {
+          if (req.files[i].size > 200 * 1024) {
+            return res.status(400).send({
+              message: "File size exceeds the limit of 200KB",
+              success: false,
+            });
+          }
+          const fileRef = ref(
+            storage,
+            "post/" +
+              req.token.id +
+              "-" +
+              Date.now() +
+              Math.floor(Math.random() * 100) +
+              "-postimage.png"
+          );
+          const metadata = {
+            contentType: req.files[i].mimetype,
+            date: Date.now(),
+          };
+          const snapshot = await uploadBytesResumable(
+            fileRef,
+            req.files[i].buffer,
+            metadata
+          );
+          var photoURL = await getDownloadURL(snapshot.ref);
+          imagesUrl.push({ url: photoURL });
+        } else if (req.files[i].mimetype.includes("video")) {
+          if (req.files[i].size > 9000 * 1024) {
+            return res.status(400).send({
+              message: "File size exceeds the limit of 9MB",
+              success: false,
+            });
+          }
+          const fileRef = ref(
+            storage,
+            "post/" +
+              req.token.id +
+              "-" +
+              Date.now() +
+              Math.floor(Math.random() * 100) +
+              "-postvideo.mp4"
+          );
+          const metadata = {
+            contentType: req.files[i].mimetype,
+            date: Date.now(),
+          };
+          const snapshot = await uploadBytesResumable(
+            fileRef,
+            req.files[i].buffer,
+            metadata
+          );
+          var videoURL = await getDownloadURL(snapshot.ref);
+          videosUrl.push({ url: videoURL });
         }
-        const fileRef = ref(
-          storage,
-          "post/" +
-            req.token.id +
-            "-" +
-            Date.now() +
-            Math.floor(Math.random() * 100) +
-            "-postimage.png"
-        );
-        const metadata = {
-          contentType: req.files[i].mimetype,
-          date: Date.now(),
-        };
-        const snapshot = await uploadBytesResumable(
-          fileRef,
-          req.files[i].buffer,
-          metadata
-        );
-        var photoURL = await getDownloadURL(snapshot.ref);
-        imagesUrl.push({ url: photoURL });
       }
     }
     req.body.creater = req.token.id;
     req.body.imageUrl = imagesUrl;
+    req.body.videoUrl = videosUrl;
     var post = new Post(req.body);
     post = await post.save();
     post = await post.populate("creater", [
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -3,10 +3,14 @@ import authenticateUser from "../middleware/authenticateUser.js";
 import multer from "multer";
 import { uploadPost } from "../controller/post.controller.js";
 
-const upload = multer().array('images')
+const upload = multer().array("files");
 
 const router = express.Router();
 
-router.post("/upload", [authenticateUser, upload], uploadPost);
+router.post(
+  "/upload",
+  [authenticateUser, upload],
+  uploadPost
+);
 
 export default router;
